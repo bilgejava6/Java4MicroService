a3b0864a96162eed0ca46466867fd7680b7eac9b
Muhammet Ali KAYA <info@beemeapp.com>
2023-01-04 13:53:42 +0300

    GlobalExceptionHandlar sınıfının eksiklikleri giderildi.

--- auth-microservice/src/main/java/com/muhammet/exception/GlobalExceptionHandlar.java
+++ auth-microservice/src/main/java/com/muhammet/exception/GlobalExceptionHandlar.java
@@ -1,11 +1,21 @@
 package com.muhammet.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.muhammet.exception.ErrorType.*;
 @ControllerAdvice
 public class GlobalExceptionHandlar {
 
@@ -20,6 +30,66 @@ public class GlobalExceptionHandlar {
     @ResponseBody
     public ResponseEntity<ErrorMessage> handlerAuthMicroseviceException(AuthMicroserviceException exception){
         return new ResponseEntity<>(createErrorMessage(exception,exception.getErrorType()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handleMessageNotReadableException(
+            HttpMessageNotReadableException exception) {
+        ErrorType errorType = BAD_REQUEST_ERROR;
+        return new ResponseEntity<>(createErrorMessage(exception,errorType), errorType.getHttpStatus());
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handleInvalidFormatException(
+            InvalidFormatException exception) {
+        ErrorType errorType = BAD_REQUEST_ERROR;
+        return new ResponseEntity<>(createErrorMessage(exception,errorType), errorType.getHttpStatus());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handlePSQLException(
+            DataIntegrityViolationException exception) {
+        ErrorType errorType = REGISTER_KULLANICIADI_KAYITLI;
+        return new ResponseEntity<>(createErrorMessage(exception,errorType), errorType.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handleMethodArgumentMisMatchException(
+            MethodArgumentTypeMismatchException exception) {
+
+        ErrorType errorType = BAD_REQUEST_ERROR;
+        return new ResponseEntity<>(createErrorMessage(exception,errorType), errorType.getHttpStatus());
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handleMethodArgumentMisMatchException(
+            MissingPathVariableException exception) {
+
+        ErrorType errorType = BAD_REQUEST_ERROR;
+        return new ResponseEntity<>(createErrorMessage(exception,errorType), errorType.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public final ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+
+        ErrorType errorType = BAD_REQUEST_ERROR;
+        List<String> fields = new ArrayList<>();
+        exception
+                .getBindingResult()
+                .getFieldErrors()
+                .forEach(e -> fields.add(e.getField() + ": " + e.getDefaultMessage()));
+        ErrorMessage errorMessage = createErrorMessage(exception,errorType);
+        errorMessage.setFields(fields);
+        return new ResponseEntity<>(errorMessage, errorType.getHttpStatus());
     }
 
     /**
